01ceb7129b5a107bd48b38c5aac2dc98e82e5538
Nelson Bryan <201913582B@utea.edu.pe>
2022-12-28 15:20:26 -0500

    first commit

---
+++ lib/pages/detallepokemon.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+import 'package:app_pokemon/models/pokemondole.dart';
+import 'package:app_pokemon/ui/general/colors.dart';
+import '../ui/widget/datositem.dart';
+import '../ui/widget/itemtypo.dart';
+
+class DetailPage extends StatelessWidget {
+  PokemonModel pokemon;
+
+  DetailPage({required this.pokemon});
+
+  @override
+  Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      backgroundColor: colorsPokemon[pokemon.type.first],
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.favorite_border_outlined),
+          ),
+        ],
+      ),
+      body: Stack(
+        children: [
+          Positioned(
+            top: height * 0.1,
+            right: -30,
+            child: Image.asset(
+              'assets/images/pokeball.png',
+              height: 200.0,
+              color: Colors.white.withOpacity(0.26),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      pokemon.name,
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 36.0),
+                    ),
+                    SizedBox(
+                      height: 6.0,
+                    ),
+                    Row(
+                      children: pokemon.type
+                          .map((e) => ItemTypeWidget(text: e))
+                          .toList(),
+                    )
+                  ],
+                ),
+                Text(
+                  "#${pokemon.numPokemon}",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )
+              ],
+            ),
+          ),
+          Column(
+            children: [
+              const Expanded(
+                flex: 4,
+                child: SizedBox(),
+              ),
+              Expanded(
+                flex: 8,
+                child: Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30.0),
+                      topRight: Radius.circular(30.0),
+                    ),
+                  ),
+                  child: Stack(
+                    clipBehavior: Clip.none,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(22.0),
+                        child: Column(
+                          children: [
+                            const Text(
+                              "About Pokemon",
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            ItemDataWidget(
+                              title: "height",
+                              data: pokemon.heigth,
+                            ),
+                            ItemDataWidget(
+                              title: "Weith",
+                              data: pokemon.weight,
+                            ),
+                            ItemDataWidget(
+                              title: "Candy",
+                              data: pokemon.candy,
+                            ),
+                            ItemDataWidget(
+                              title: "Egg",
+                              data: pokemon.egg,
+                            ),
+                          ],
+                        ),
+                      ),
+                      Positioned.fill(
+                        top: -90,
+                        child: Align(
+                          alignment: Alignment.topCenter,
+                          child: Image.network(
+                            pokemon.img,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/ui/widget/itempokemon.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:app_pokemon/models/pokemondole.dart';
+import 'package:app_pokemon/pages/detallepokemon.dart';
+import 'package:app_pokemon/ui/general/colors.dart';
+import 'package:app_pokemon/ui/widget/itemtypo.dart';
+
+class ItemPokemonWidget extends StatelessWidget {
+  /*String name;
+  String image;
+  List<String> types;*/
+  PokemonModel pokemon;
+  ItemPokemonWidget({
+    /*required this.name,
+    required this.image,
+    required this.types,*/
+    required this.pokemon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: ((context) => DetailPage(
+                      pokemon: pokemon,
+                    ))));
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          color: colorsPokemon[pokemon.type.first],
+          borderRadius: BorderRadius.circular(18.0),
+        ),
+        child: Stack(
+          children: [
+            Positioned(
+              bottom: -20,
+              right: -20,
+              child: Image.asset(
+                "assets/images/pokeball.png",
+                height: 120.0,
+                color: Colors.white.withOpacity(0.2),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 20,
+                horizontal: 12,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    pokemon.name,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16.0,
+                    ),
+                  ),
+                  /*Column(
+                    children: types.map((e) => ItemTypeWidget()).toList(),
+                  )*/
+                  ...pokemon.type
+                      .map((e) => ItemTypeWidget(
+                            text: e,
+                          ))
+                      .toList(),
+                ],
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              right: 0,
+              child: Image.network(pokemon.img),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
